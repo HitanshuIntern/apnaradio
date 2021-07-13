71437d624eca3192c714850005fca4fdc5e8059d
Hitanshu Soni <hitanshusoni10@gmail.com>
2021-07-13 20:23:53 +0530

    radio

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import Radio from "./Radio"
+function App() {
+  return (
+    <div className="App">
+    <h1>Apna Radio Player</h1>
+    <h1>Pick a gerne, choose a station, start listening</h1>
+    <Radio/>
+    </div>
+        );
+}
+
+export default App;
